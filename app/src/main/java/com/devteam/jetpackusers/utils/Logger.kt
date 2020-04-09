54319721d81051709f33166dedf09ae664e429d7
Krishnaprasad N <krishnaprasad.n@glowtouch.com>
2020-04-09 11:19:53 +0530

    Committing the code

---
+++ app/src/main/java/com/devteam/jetpackusers/utils/Logger.kt
@@ -0,0 +1,35 @@
+package com.devteam.jetpackusers.utils
+
+import android.os.Build
+import android.util.Log
+
+/**
+ * Logger utility class. If the DEBUG flag is false, then it does not log the msg
+ *
+ */
+object Logger {
+    private const val TAG: String = "App"
+    private const val DEBUG = true
+
+    // general purpose debuggable log method
+    fun d(msg: String) {
+        log("**** -> $msg")
+    }
+
+    // logs thread details
+    fun logThreadDetails(from: String) {
+        log(
+            "*** $from  --> Thread # ${Thread.currentThread().id} with " +
+                    "name = ${Thread.currentThread().name}"
+        )
+    }
+
+    // this is a special method to log the msg without the influence of DEBUG flag
+    fun logCritical(msg: String) {
+        Log.d(TAG, msg)
+    }
+
+    private fun log(msg: String) {
+        if (DEBUG) Log.d(TAG, msg)
+    }
+}
